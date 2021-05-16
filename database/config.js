84bf8050bdae5a0e037b1691a93d8034925bfb52
Ramon Barros <pbarrosramon@gmail.com>
2021-05-16 11:59:13 -0300

    add public config file to version1

---
+++ database/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    uri: 'mongodb+srv://public:public@brazilavrank.2ccxo.mongodb.net/ValleyAlliance?retryWrites=true&w=majority',
+    users: "users",
+    drivers: "Drivers",
+    raceRecords: "RaceRecord",
+    raceResults: "RaceResult",
+    rating: "HistRating",
+    ranking: "HistRank"
+}
